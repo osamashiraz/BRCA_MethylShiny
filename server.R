768ab76e6f73c40caf0a563dde39619953cb6876
Osama Shiraz Shah <osamashirazshah@gmail.com>
2020-04-18 21:37:07 -0400

    updated comments

--- server.R
+++ server.R
@@ -1,49 +1,46 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 # LOAD LIBRARIES
 library(shiny)
+options(repos = BiocManager::repositories())
 
 
 # LOAD DATA
-## META
-  load(file = "Inputs/probeMapByUniqueGenes.Rdata")
-  GeneMappingGroups=read.csv("Inputs/GeneMappingGroups.csv")
-  
-  
+## PROBE MAPPING DATA
+load(file = "Inputs/probeMapByUniqueGenes.Rdata")
+GeneMappingGroups=read.csv("Inputs/GeneMappingGroups.csv")
+
+
 ## CLINICAL DATA
-  load("Inputs/clinData.Rdata")
-  
+load("Inputs/clinData.Rdata")
+
 ## REDUCED HM450 & CPM DATA
-  load(file = "Inputs/ReducedCPM.Rdata")
-  load(file = "Inputs/ReducedHM450_Body.Rdata")
-  load(file = "Inputs/ReducedHM450_TSS.Rdata")
-  
-  
+load(file = "Inputs/ReducedCPM.Rdata")
+load(file = "Inputs/ReducedHM450_Body.Rdata")
+load(file = "Inputs/ReducedHM450_TSS.Rdata")
+
+## PLOT THEMING FUNCTION
 myTheme=function(titleFont=15,generalFont=10,legendFont=8,axisFont=8,subtitleFont=12){
   theme(axis.text = element_text(face = "bold",color = "black",size = axisFont),
-              plot.title = element_text(face="bold",colour = "darkblue",size = titleFont),
-              title = element_text(face="bold",colour = "darkblue",size = generalFont), 
-              legend.text = element_text(face="bold",color="darkblue",size=legendFont),
-              plot.subtitle =element_text(color="red",size = subtitleFont))
+        plot.title = element_text(face="bold",colour = "darkblue",size = titleFont),
+        title = element_text(face="bold",colour = "darkblue",size = generalFont), 
+        legend.text = element_text(face="bold",color="darkblue",size=legendFont),
+        plot.subtitle =element_text(color="red",size = subtitleFont))
 }
 
   
-# FUNCTIONS
+## PLOTTING FUNCTIONS
+
+
+# FUNCTION 1 - PROBE CATEGORY PLOTS
 metaDataPlot=function(type){
   library(ggplot2)
+  
+  # PLOT 1
   if (type == "1"){
     p=ggplot(GeneMappingGroups,aes(reorder(Var1,Freq), Freq/sum(GeneMappingGroups$Freq), fill=Var1))+
       geom_bar(stat = "identity",show.legend = F)+xlab("Probe-Gene Mapping Categories")+ylab("Proportion of Probes")+geom_text(aes(label=Freq))
-      theme_classic()+myTheme(generalFont = 20,axisFont = 15)
+    theme_classic()+myTheme(generalFont = 20,axisFont = 15)
     return(p)
-  }else{
+  }else{ # PLOT 2
     library(ComplexHeatmap)
     library(circlize)
     mat=as.data.frame(probeMappingTest)
@@ -58,20 +55,20 @@ metaDataPlot=function(type){
     column_ha = rowAnnotation(`Region Proportions` =anno_barplot( round(colSums(mat)/nrow(mat),2),gp = gpar(fill="brown")),width = unit(2,"cm"))
     row_ha = HeatmapAnnotation(`Probes per gene`= anno_barplot(probeCounts),height = unit(2,"cm"))
     marks = HeatmapAnnotation(foo = anno_mark(side = "left",at = which(probeCounts>300), 
-                                          labels_gp = gpar(fontsize=10),labels = names(which(probeCounts>300))))
+                                              labels_gp = gpar(fontsize=10),labels = names(which(probeCounts>300))))
     
     ht=Heatmap(t(mat),col = colorRamp2(c(0,1), c("black", "yellow")),cluster_columns = F, cluster_rows = F,
-            show_column_names = F,right_annotation = column_ha, top_annotation = row_ha,
-            name = "Probe Proportion",column_title = "Genes",bottom_annotation = marks)
+               show_column_names = F,right_annotation = column_ha, top_annotation = row_ha,
+               name = "Probe Proportion",column_title = "Genes",bottom_annotation = marks)
     return(ht)
   }
 }
 
 
-## GENE PLOTS
+# FUNCTION 2 - GENE PLOTS
 makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
   print("here1")
-  
+  # HELPER FUNCTION 1 - GET PATIENT IDS FOR SELECTED CLINICAL VARIABLE
   makeCompareByGroups=function(clinData, compareBy="ER Status"){
     if (compareBy == "ER Status"){
       return(subset(clinData,`ER IHC` %in% c("Positive","Negative","Adjacent Normal"))$Case.ID)
@@ -81,7 +78,7 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
       return(subset(clinData,PAM50 %in% c("LumA","LumB","Her2","Basal","Normal","Adjacent Normal"))$Case.ID)
     }
   }
-  
+  # HELPER FUNCTION 2 - GET COLUMN SPLITTING ORDER
   makeColsplitGroups=function(compareBy="ER Status",sampleList){
     if (compareBy == "ER Status"){
       field="ER IHC"
@@ -92,7 +89,7 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
     }
     return(data.frame(factor(clinData[sampleList,field])))
   }
-  
+  # HELPER FUNCTION 3 - GENERATE HEATMAP ANNOTATIONS
   makeHeatmapAnnotation=function(compareBy="ER Status",sampleList){
       if (compareBy == "ER Status"){
         annot=HeatmapAnnotation(ER = factor(clinData[sampleList,"ER IHC"]),na_col = "gray", height = unit(1, "cm"),
@@ -106,7 +103,7 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
       }
       return(annot)
   }
-  
+  # HELPER FUNCTION 4 - GET CLINICAL ANNOTATIONS
   chooseClinAnnot=function(clinData,sampleList,compareBy){
     if (compareBy == "ER Status"){
       return(subset(clinData[sampleList,],`ER IHC` %in% c("Positive","Negative","Adjacent Normal"))[,c("Case.ID","ER IHC")])
@@ -116,8 +113,8 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
       return(subset(clinData[sampleList,],PAM50 %in% c("LumA","LumB","Her2","Basal","Normal","Adjacent Normal"))[,c("Case.ID","PAM50")])
     }
   }
-  
-  ggplotRegression <- function (fit) {
+  # HELPER FUNCTION 5 - PLOT REGRESSION LINE
+  regressionStat <- function (fit) {
     labs(title=gene,subtitle = paste0("Adj R2 = ",round(summary(fit)$adj.r.squared, 2),
                                       ", Slope =",round(fit$coef[[2]], 2),
                                       ", P =",round(summary(fit)$coef[2,4], 3)))
@@ -128,15 +125,16 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
   # SAMPLE LIST
   sampleList=Reduce(intersect, list(makeCompareByGroups(clinData,compareBy),colnames(TCGA_BRCA_CPM_Reduced),colnames(HM450_TSS_Avg_Reduced)))
   
-  print("here1")
   
+  # GET METHYLATION AND GENE EXPRESSION DATA
   matHM450=cbind(HM450_TSS_Avg_Reduced[gene,sampleList],HM450_Body_Avg_Reduced[gene,sampleList]); 
   matCPM=as.matrix(TCGA_BRCA_CPM_Reduced[gene,sampleList]);
   
+  
+  # HEATMPA PLOT
   if (type == "1"){
     library(ComplexHeatmap)
     library(circlize)
-    print("here2")
     
     mat=apply(cbind(matCPM,matHM450),2, FUN = function(x) scale(x,center = T, scale = T))
     colnames(mat)=paste0(c("Gene Expression","TSS Methylation","Gene Body Methylation"))
@@ -149,7 +147,6 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
     ht=Heatmap(t(mat),row_names_side = "left",show_row_names = T, name = "Signal Intensity",column_title  = "Samples",
             top_annotation = column_ha,show_heatmap_legend = T,column_split = colSplit)
     
-    print("here3")
     return(ht)
     }
   
@@ -157,10 +154,12 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
   library(RColorBrewer)
   myColors <- brewer.pal(6,"Set1")
   
-  genePlot=function(df,cols=NULL){
+  
+  ## SCATTER PLOT FUNCTION
+  scatterPlot=function(df,cols=NULL){
     p=ggplot(df,aes(df[,1],df[,2]))+geom_point(aes(color=df[,3]),alpha=0.5,size=4)+theme_classic()+
       guides(size=10, color=guide_legend(title = compareBy))+ geom_smooth(method = "lm", color="black",se = T,size=1)+
-      ggplotRegression(lm(df[,1] ~ df[,2], data = df))
+      regressionStat(lm(df[,1] ~ df[,2], data = df))
     if(length(cols)>0){ 
       p=p+scale_colour_manual(values = cols)
     }else(
@@ -170,21 +169,21 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
   }
   
   
-  
+  # GENERATE TSS-GENE EXP SCATTER PLOTS
   if (type == "2"){
     annot=chooseClinAnnot(clinData,sampleList,compareBy)
     p2DF=data.frame(exp=matCPM[annot[,1],1], TSS=matHM450[annot[,1],1], Body=matHM450[annot[,1],2],annot=annot[,2])
     
     
     if (!DoColSplit){
-      # TSS MAIN
-      TSSplot=genePlot(p2DF[,c("TSS","exp","annot")])+xlab("TSS Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+myTheme(generalFont = 15,axisFont = 10)
+      # MAIN PLOT
+      TSSplot=scatterPlot(p2DF[,c("TSS","exp","annot")])+xlab("TSS Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+myTheme(generalFont = 15,axisFont = 10)
       return(TSSplot)
     }else{
-      # TSS SUB
+      # SUB PLOTS
       TSSsubplots=lapply(unique(p2DF$annot), function(x){
           col=myColors[match(x,unique(p2DF$annot))]
-          genePlot(p2DF[p2DF$annot %in% x,][,c("TSS","exp","annot")],cols = col)+xlab("TSS Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+
+          scatterPlot(p2DF[p2DF$annot %in% x,][,c("TSS","exp","annot")],cols = col)+xlab("TSS Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+
             myTheme(generalFont = 8,subtitleFont = 8)
       })
       names(TSSsubplots)=unique(p2DF$annot)
@@ -192,20 +191,20 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
     }
   }
   
-  
+  # GENERATE GENE BODY-GENE EXP SCATTER PLOTS
   if (type == "3"){
     annot=chooseClinAnnot(clinData,sampleList,compareBy)
     p2DF=data.frame(exp=matCPM[annot[,1],1], TSS=matHM450[annot[,1],1], Body=matHM450[annot[,1],2],annot=annot[,2])
     
     if (!DoColSplit){
-      # BODY MAIN
-      Bodyplot=genePlot(p2DF[,c("Body","exp","annot")])+xlab("Body Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+myTheme(generalFont = 15,axisFont = 10)
+      # MAIN PLOT
+      Bodyplot=scatterPlot(p2DF[,c("Body","exp","annot")])+xlab("Body Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+myTheme(generalFont = 15,axisFont = 10)
       return(Bodyplot)
     }else{
-      # BODY SUB
+    # SUB PLOTS
     Bodysubplots=lapply(unique(p2DF$annot), function(x){
       col=myColors[match(x,unique(p2DF$annot))]
-      genePlot(p2DF[p2DF$annot %in% x,][,c("Body","exp","annot")],cols = col)+xlab("Body Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+
+      scatterPlot(p2DF[p2DF$annot %in% x,][,c("Body","exp","annot")],cols = col)+xlab("Body Methylation (B-Value)")+ylab("Expression (Counts Per Million)")+
         myTheme(generalFont = 8,subtitleFont = 8)
     })
     names(Bodysubplots)=unique(p2DF$annot)  
@@ -216,7 +215,9 @@ makeGenePlot=function(gene="CDH1", compareBy, DoColSplit=T,type="1"){
 }
 
 
-# Define server logic required to draw a histogram
+
+
+## SERVER FUNCTION
 shinyServer(function(input, output, session) {
   
   # META DATA PLOT FUNCTION
